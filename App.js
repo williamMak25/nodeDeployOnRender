3a2295e9f4433f4e7452430136b26e6e2520211c
williamMak25 <waiyanwinlwin25@gmail.com>
2023-07-19 22:41:41 +0630

    test commit

--- App.js
+++ App.js
@@ -5,7 +5,12 @@ const express = require("express")
 
 const App = express();
 App.use(express.json())
-
+App.get("/",(req,res)=>{
+    res.json({
+        name:"waiyan",
+        age:"22"
+    })
+})
 App.listen(port,()=>{
     console.log("server is connected")
 })
